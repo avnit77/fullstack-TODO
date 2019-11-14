9854b0087f16028b497c37d675fd80b21762dcf7
Tali Avni <tali@aristo7.com>
2019-11-14 10:01:52 -0800

    updated server.js

--- server.js
+++ server.js
@@ -10,6 +10,30 @@ const client = require('./lib/client');
 client.connect();
 
 // Application Setup
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+        SELECT id, email, hash
+            FROM users
+            WHERE email = $1;
+        `,
+        [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        console.log(user);
+        return client.query(`
+            INSERT into users (email, hash)
+            VALUES ($1, $2)
+            RETURNING id, email;
+        `,
+        [user.email, hash]
+        ).then(result => result.rows[0]);
+    }
+});
+
 const app = express();
 const PORT = process.env.PORT;
 app.use(morgan('dev')); // http logging
